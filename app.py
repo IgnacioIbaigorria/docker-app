a9642f2eb3746229316485c70ecc222f93515d0b
Ignacio Ibaigorria <153262013+IgnacioIbaigorria@users.noreply.github.com>
2024-11-13 15:56:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
+# app.py
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 from pytesseract import pytesseract
 from PIL import Image
-import os
+import base64
+import io
 
 app = Flask(__name__)
-CORS(app)  # Habilita CORS para permitir peticiones externas
 
 # Configura la ruta de Tesseract en Docker
 pytesseract.tesseract_cmd = "/usr/bin/tesseract"
 
 @app.route('/extract_text', methods=['POST'])
 def extract_text():
-    print("Solicitud recibida")
-    print("Archivos recibidos:", request.files)
     try:
-        # Verificar si se ha enviado un archivo
-        if 'image' not in request.files:
-            return jsonify({"error": "No se envió ningún archivo"}), 400
+        data = request.get_json()
+        if not data or 'image' not in data:
+            return jsonify({"error": "No se envió ninguna imagen"}), 400
 
-        # Leer el archivo de imagen enviado
-        file = request.files['image']
-        image = Image.open(file)
+        # Decodificar la imagen base64
+        image_data = base64.b64decode(data['image'])
+        image = Image.open(io.BytesIO(image_data))
 
         # Extraer texto con Tesseract OCR
         extracted_text = pytesseract.image_to_string(image)
