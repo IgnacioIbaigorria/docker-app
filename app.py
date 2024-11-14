f268ead02c11200f99bc5153197f9e139278d323
Ignacio Ibaigorria <153262013+IgnacioIbaigorria@users.noreply.github.com>
2024-11-14 19:10:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-# app.py
+import os
+import json
 from flask import Flask, request, jsonify
 from google.cloud import vision
 from google.oauth2 import service_account
@@ -6,39 +7,37 @@ from io import BytesIO
 
 app = Flask(__name__)
 
-# Inicializar el cliente de Google Cloud Vision
-credentials = service_account.Credentials.from_service_account_file(
-    "/app/plasma-system-441721-h5-e71c8d0472b7.json"
-)
+# Crea un archivo temporal para las credenciales usando el contenido de la variable de entorno
+credentials_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
+credentials_path = "/tmp/google_credentials.json"
+
+# Escribe el contenido de la variable en un archivo
+with open(credentials_path, "w") as f:
+    f.write(credentials_json)
+
+# Cargar las credenciales
+credentials = service_account.Credentials.from_service_account_file(credentials_path)
 client = vision.ImageAnnotatorClient(credentials=credentials)
 
 @app.route('/extract_text', methods=['POST'])
 def extract_text():
     try:
-        # Verificar si se ha enviado un archivo
         if 'image' not in request.files:
             return jsonify({"error": "No se envió ningún archivo"}), 400
 
-        # Leer el archivo de imagen enviado
         file = request.files['image']
         image_content = file.read()
 
-        # Crear la imagen para Google Cloud Vision
         image = vision.Image(content=image_content)
-
-        # Realizar la detección de texto
         response = client.text_detection(image=image)
         texts = response.text_annotations
 
-        # Extraer el texto principal si existe
         extracted_text = texts[0].description if texts else ""
         print("Texto extraído:", extracted_text)
 
-        # Verificar si hubo errores en la respuesta
         if response.error.message:
             return jsonify({"error": response.error.message}), 500
 
-        # Retornar el texto extraído
         return jsonify({"text": extracted_text}), 200
     except Exception as e:
         print("Error:", e)
